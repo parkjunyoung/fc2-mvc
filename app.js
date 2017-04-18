810512ece8c3b524ad71ea0e2de3fd62b914b532
parkjunyoung <pjoon86@naver.com>
2017-04-18 18:26:49 +0900

    로그인 구현

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 
+//flash  메시지 관련
+var flash = require('connect-flash');
+
+//passport 로그인 관련
+var passport = require('passport');
+var session = require('express-session');
+
 //MongoDB 접속
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
@@ -43,6 +50,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //업로드 path 추가
 app.use('/uploads', express.static('uploads'));
+
+
+//session 관련 셋팅
+app.use(session({
+    secret: 'fastcampus',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 2000 * 60 * 60 //지속시간 2시간
+    }
+}));
+
+//passport 적용
+app.use(passport.initialize());
+app.use(passport.session());
+
+//플래시 메시지 관련
+app.use(flash());
 
 app.use('/', index);
 app.use('/users', users);
