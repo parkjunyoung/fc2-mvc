b02b1000c8763f5ee7dcfc1fd2216ea563f59e22
parkjunyoung <pjoon86@naver.com>
2017-04-19 12:58:32 +0900

    페이스북 로그인

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var posts = require('./routes/posts');
 var accounts = require('./routes/accounts');
+var auth = require('./routes/auth');
 
 var app = express();
 
@@ -73,6 +74,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/posts', posts);
 app.use('/accounts', accounts);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
